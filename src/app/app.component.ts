78979d979eb5abdd13746383e708cfd0ae48c3c2
bitxuji <bitxuji@gmail.com>
2023-11-23 17:34:20 +0900

    CheckBox of angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  isChecked = true;
+  onChange(event: any) {
+    console.log(event);
+  }
 }
